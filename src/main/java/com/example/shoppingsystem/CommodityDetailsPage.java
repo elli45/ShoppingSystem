5b9e34c3b0d6c8027a4bdc7fd820b7a913465ea0
= <=>
2023-07-08 08:27:18 +0430

    Creating CommodityDetailsPage

---
+++ src/main/java/com/example/shoppingsystem/CommodityDetailsPage.java
@@ -0,0 +1,13 @@
+package com.example.shoppingsystem;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class CommodityDetailsPage implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
